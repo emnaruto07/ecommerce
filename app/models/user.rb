b9c7a6fc389fd6648b1b7d08425f917c3fa6c605
emnaruto07 <shazebvps@gmail.com>
2024-04-18 01:23:01 +0300

    added uuid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_create :set_uuid
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  # create a function which set uuid to user
+  def set_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
